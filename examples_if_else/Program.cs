6d3c35efc212183de56f55f39daac1dc61da25c4
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-08 15:34:05 +0300

    examples if else

---
+++ examples_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это Маша");
+}
+else
+{
+    Console.Write("Привет это ");
+    Console.WriteLine(userName);
+}

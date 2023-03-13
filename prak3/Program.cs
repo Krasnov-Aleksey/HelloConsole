2d94344e9806665c23eda2a61adfb538f820265e
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-13 15:18:20 +0300

    findSecondMaxRange

--- prak3/Program.cs
+++ prak3/Program.cs
@@ -34,12 +34,55 @@ int findMaxValue(int[] col)
     return max;
 }
 
+// Поиск максимального значение в диапазоне
+int findMaxValueRange(int[]col, int begin, int end)
+{
+    int max=col[begin];
+    while(begin<=end)
+    {
+        if (col[begin]>max)
+        {
+            max=col[begin];
+        }
+        
+    begin++;
+    }
+    return max;
+}
+
+// Поиск второго максимального значение в диапазоне
+int findSecondMaxValueRange(int[] col, int begin, int end) //,int ignore
+{
+    int ignore = findMaxValueRange(col, begin, end);
+    int max2 = 0;
+        
+    while (begin <= end)
+    {
+        
+        if (col[begin] < ignore)
+        {
+            if(col[begin]>max2)
+            {
+                max2=col[begin];
+            }
+        }
+
+        begin++;
+
+    }
+    return max2;
+}
 
 
 
 
-int [] array={1,2,3,4,5,6,7,0,8,9,10};
-int position=findValueIndex(array,0);
-int max=findMaxValue(array);
-Console.WriteLine(position);
-Console.WriteLine(max);
+int [] array={1,12,31,54,15,6,7,0,81,9,10};
+// int position=findValueIndex(array,0);
+// int max=findMaxValue(array);
+// Console.WriteLine(position);
+// Console.WriteLine(max);
+//int maxrang = findMaxValueRange(array,1,9);
+// Console.WriteLine(maxrang);
+int maxsecondrang = findSecondMaxValueRange(array,1,10);
+//Console.WriteLine(maxrang);
+Console.WriteLine(maxsecondrang);

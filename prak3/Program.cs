cb36689f376d0b9fae707bf80aa5f3bffb5c1a27
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-13 12:42:15 +0300

    findValueIndex

--- prak3/Program.cs
+++ prak3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет");
+﻿Console.Clear();
+
+int findValueIndex(int[] col, int value)
+{
+    int length=col.Length;
+    int index=0;
+    while(index<length)
+    {
+        if (col[index]==value)
+        {
+            return index;
+        }
+        
+    index++;
+    }
+return -1;
+}
+
+int [] array={1,2,3,4,5,6,7,0,8,9,10};
+int position=findValueIndex(array,12);
+Console.WriteLine(position);
+

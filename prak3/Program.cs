aa0fbe82f82a6a2c445a3bee76179df5f8dec1ca
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-13 15:31:05 +0300

    findSecondValueRange до 0

--- prak3/Program.cs
+++ prak3/Program.cs
@@ -51,7 +51,7 @@ int findMaxValueRange(int[]col, int begin, int end)
 }
 
 // Поиск второго максимального значение в диапазоне
-int findSecondMaxValueRange(int[] col, int begin, int end) //,int ignore
+int findSecondMaxValueRange(int[] col, int begin, int end) 
 {
     int ignore = findMaxValueRange(col, begin, end);
     int max2 = 0;
@@ -74,15 +74,8 @@ int findSecondMaxValueRange(int[] col, int begin, int end) //,int ignore
 }
 
 
-
-
-int [] array={1,12,31,54,15,6,7,0,81,9,10};
-// int position=findValueIndex(array,0);
-// int max=findMaxValue(array);
-// Console.WriteLine(position);
-// Console.WriteLine(max);
-//int maxrang = findMaxValueRange(array,1,9);
-// Console.WriteLine(maxrang);
-int maxsecondrang = findSecondMaxValueRange(array,1,10);
-//Console.WriteLine(maxrang);
+int [] array={32,12,31,54,15,6,7,0,81,9,10};
+int find=0;
+int position0=findValueIndex(array,find);
+int maxsecondrang = findSecondMaxValueRange(array,0,position0);
 Console.WriteLine(maxsecondrang);

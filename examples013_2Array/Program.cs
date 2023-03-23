544efea2ea8fa89818a0f427e519c4e44fcde3cc
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-23 15:17:32 +0300

    2Array

---
+++ examples013_2Array/Program.cs
@@ -0,0 +1,25 @@
+﻿void InputDArray(int [,] col)
+{
+    for (int i=0; i<col.GetLength(0);i++)
+    {
+        for (int j=0; j<col.GetLength(1);j++)
+        {
+            col[i,j]=new Random().Next(1,10);
+            Console.Write(col[i,j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+int [,] arr= new int [3,5];
+InputDArray(arr);
+
+
+
+
+
+
+
+
+

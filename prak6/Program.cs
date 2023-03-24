6125173f0e242561ac2302e7f31a4eb59995daf1
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-24 14:44:43 +0300

    prak6

--- prak6/Program.cs
+++ prak6/Program.cs
@@ -1,3 +1,19 @@
-﻿
+﻿// Console.Clear();
+// Console.Write("Введите 1 сторону триугольника ");
+// int n1=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 1 сторону триугольника ");
+// int n2=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 1 сторону триугольника ");
+// int n3=Convert.ToInt32(Console.ReadLine());
+// if (n1<n2+n3&& n2<n1+n3 && n3<n1+n2) 
+//     Console.Write("Yes");
+// else
+//     Console.Write("No");
 Console.Clear();
-
+Console.Write("Введите число ");
+int n=Convert.ToInt32(Console.ReadLine());
+int n2=1;
+for (int i=0;i<=n;i++)
+{
+    Console.WrineLine(i);
+}
